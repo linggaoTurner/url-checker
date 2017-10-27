f4596e71c104bad7a5e08ca997d6c6b6fbbd94f4
linggaoTurner <ling.gao@turner.com>
2017-10-27 10:48:31 -0400

    checker indentation issue

--- checker.py
+++ checker.py
@@ -14,20 +14,45 @@ def urlchecker(url,frequency):
     sched.add_job(schedulejob,'interval',[url], seconds = float(frequency))
 
 def schedulejob(url):
-    r = requests.get(url)
     health = ''
+    status = ''
+    try:
+        r = requests.get(url, timeout=6)
+        status = r.status_code
 
-    if urlInventory.select(urlInventory.expectedStatus).where(urlInventory.urlDomain == url) == r.status_code and urlInventory.select(urlInventory.expectedString).where(urlInventory.urlDomain == url) == r.text:
-        health = 'SUCCESS'
-    else:
+        if urlInventory.select(urlInventory.expectedStatus).where(urlInventory.urlDomain == url) == status and urlInventory.select(urlInventory.expectedString).where(urlInventory.urlDomain == url) == r.text:
+            health = 'SUCCESS'
+        else:
+            health = 'FAILURE'
+    except requests.exceptions.InvalidSchema as e:
+        print("Invalid Schema")
         health = 'FAILURE'
+        storeLogs(url,status,health)
+    except requests.exceptions.ReadTimeout as e:
+        print("Time Out")
+        health = 'FAILURE'
+        storeLogs(url,status,health)
+        except requests.exceptions.ConnectionError as e:
+        print("Connection Issue")
+        health = 'FAILURE'
+        storeLogs(url,status,health)
+    except requests.exceptions.TooManyRedirects as e:
+        print("Too Many Redirects")
+        health = 'FAILURE'
+        storeLogs(url,status,health)
+    except requests.exceptions.ChunkedEncodingError as e:
+        print("Encoding Error")
+        health = 'FAILURE'
+        storeLogs(url,status,health)
 
-    urlLog.create(urlDomain = url,
-                   timeChecked=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                   statusCode=r.status_code,
-                   healthyState=health)
     healthlist = []
     for data in urlLog.select().order_by(urlLog.id.desc()).limit(3):
         healthlist.append(data.healthyState)
     if healthlist.count('FAILURE') == 3:
         sendEmail.sent(url)
+
+def storeLogs(url,status,health):
+    urlLog.create(urlDomain = url,
+                  timeChecked=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                  statusCode=status,
+                  healthyState=health)
